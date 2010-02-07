cd5bbf0bc89c40f88d08d354deb4914a3cebb714
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-07 20:13:22 +0100

    Added module names as header to all module actions in rake task.

--- lib/tasks/modules.rb
+++ lib/tasks/modules.rb
@@ -14,29 +14,30 @@ namespace :modules do
     $git ||= Git::System.new git_config
   end
 
+  def show_header(git_module)
+    puts
+    puts git_module
+    puts git_module.to_s.gsub /./, '*'
+  end
+
   desc "Initialize the GIT sub-modules if necessary."
   task :init do
-    git_modules.each { |m| git.init_module(m) }
+    git_modules.each { |m| show_header m ; git.init_module(m) }
   end
 
   desc "Update the GIT sub-modules if necessary."
   task :update do
-    git_modules.each { |m| git.update_module(m) }
+    git_modules.each { |m| show_header m ; git.update_module(m) }
   end
 
   desc "Show status of the GIT sub-modules."
   task :status do
-    git_modules.each do |m|
-      puts
-      puts m
-      puts m.to_s.gsub /./, '*'
-      git.module_status(m)
-    end
+    git_modules.each { |m| show_header m ; git.module_status(m) }
   end
 
   desc "Stash changes in the GIT sub-modules."
   task :stash do
-    git_modules.each { |m| git.stash_module(m) }
+    git_modules.each { |m| show_header m ; git.stash_module(m) }
   end
 
 end
