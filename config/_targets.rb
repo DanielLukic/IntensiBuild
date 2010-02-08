ee63b08479b56ebcd60740b49ae746bb70552069
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-08 16:51:49 +0100

    Updated to use Android 1.6 for now.

--- config/_targets.rb
+++ config/_targets.rb
@@ -2,6 +2,6 @@
 # TARGET NAME                     PLATFORM GROUP  SYMBOLS
 
 Generic_JAVA =        Target.new  JAVA,           "JAVA"
-Generic_ANDROID =     Target.new  ANDROID,        "ANDROID,ANDROID15"
+Generic_ANDROID =     Target.new  ANDROID,        "ANDROID,ANDROID15,ANDROID16"
 Generic_J2ME =        Target.new  J2ME,           "J2ME,CLDC11,MIDP2"
 Nokia_J2ME =          Target.new  NOKIA,          "J2ME,CLDC11,MIDP2,NOKIA"
