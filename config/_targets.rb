34b1003fce73c9797d4618f04d44db42e85a6fd3
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-23 22:02:03 +0100

    Replaced Generic_ANDROID target with Canvas_ANDROID and Opengl_ANDROID. Used only as indicator targets for now. Added Touch_J2ME target, too.

--- config/_targets.rb
+++ config/_targets.rb
@@ -2,6 +2,8 @@
 # TARGET NAME                     PLATFORM GROUP  SYMBOLS
 
 Generic_JAVA =        Target.new  JAVA,           "JAVA"
-Generic_ANDROID =     Target.new  ANDROID,        "ANDROID,TOUCH_SUPPORTED"
+Canvas_ANDROID =      Target.new  ANDROID,        "ANDROID,TOUCH"
+Opengl_ANDROID =      Target.new  ANDROID,        "ANDROID,TOUCH,OPENGL"
 Generic_J2ME =        Target.new  J2ME,           "J2ME,CLDC11,MIDP2"
+Touch_J2ME =          Target.new  J2ME,           "J2ME,CLDC11,MIDP2,TOUCH"
 Nokia_J2ME =          Target.new  NOKIA,          "J2ME,CLDC11,MIDP2,NOKIA"
