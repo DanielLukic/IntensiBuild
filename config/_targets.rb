ddd46bc71f2e66431aa18eece56265ba408cd625
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-23 19:20:36 +0100

    Added TOUCH_SUPPORTED to the base symbols of Generic_ANDROID target.

--- config/_targets.rb
+++ config/_targets.rb
@@ -2,6 +2,6 @@
 # TARGET NAME                     PLATFORM GROUP  SYMBOLS
 
 Generic_JAVA =        Target.new  JAVA,           "JAVA"
-Generic_ANDROID =     Target.new  ANDROID,        "ANDROID"
+Generic_ANDROID =     Target.new  ANDROID,        "ANDROID,TOUCH_SUPPORTED"
 Generic_J2ME =        Target.new  J2ME,           "J2ME,CLDC11,MIDP2"
 Nokia_J2ME =          Target.new  NOKIA,          "J2ME,CLDC11,MIDP2,NOKIA"
