0f6b153ff2c29149798a9aab7bb65a22b3db7777
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-03-02 11:14:50 +0100

    Fixed increment_version.rb script to support updating version, major, minor, build and date individually and combined.

--- src/ruby/increment_version.rb
+++ src/ruby/increment_version.rb
@@ -1,65 +1,92 @@
-if ARGV.size != 2
-    puts "Usage: #{$0} filename partname"
-    puts
-    puts "Will update the version.properties file and put a three-part value in there:"
-    puts "version=<release>.<version>.<build>"
-    puts
-    puts "Example: #{$0} version.properties build"
-    puts "Example: #{$0} version.properties version"
-    puts "Example: #{$0} version.properties release"
-    puts
-    exit 1
+def now_as_date
+  Time.now.strftime('%Y-%m-%d %H:%M:%S')
+end
+
+if ARGV.size < 1
+  puts "Usage: #{$0} filename partname(s)"
+  puts
+  puts "Will update the version.properties file and put a three-part value in there:"
+  puts "version=<release>.<version>.<build>"
+  puts
+  puts "Example: #{$0} version.properties minor"
+  puts "Example: #{$0} version.properties major"
+  puts "Example: #{$0} version.properties version"
+  puts
+  exit 1
 end
 
 target_file_name = ARGV.shift
 
 if File.exist?(target_file_name)
-    version_line = File.readlines(target_file_name).select { |l| l =~ /^version/ }.first
+  lines = File.readlines(target_file_name)
+
+  version_line = lines.select { |l| l =~ /^version/ }.first || "version=0.0.0"
+  build_line = lines.select { |l| l =~ /^build/ }.first || "build=0"
+  date_line = lines.select { |l| l =~ /^date/ }.first || "date=#{now_as_date}"
+
+  old_version_string = version_line.split('=').last
+  version, major, minor = old_version_string.split('.').map { |v| v.to_i }
+
+  old_build_string = build_line.split('=').last
+  build = old_build_string.to_i
+
+  date = date_line.split('=').last
 else
-    version_line = "version=0.0.0"
+  version = major = minor = 0
+  build = 0
+  date = now_as_date
 end
 
-old_version = version_line.split('=').last
-release,version,build = old_version.split('.').map { |v| v.to_i }
+parts = ARGV.map {|part| part.downcase}
 
-part = ARGV.shift.downcase
-if part == 'release'
-    release += 1
-    version = 0
-    build = 0
-elsif part == 'version'
-    version += 1
-    build = 0
-elsif part == 'build'
-    build += 1
-else
-    show_only = true
+if parts.include?('version')
+  version += 1
+  major = 0
+  minor = 0
 end
 
-if ( build > 99 )
-    version += 1
-    build = 0
+if parts.include?('major')
+  major += 1
+  minor = 0
+end
+
+if parts.include?('minor')
+  minor += 1
+end
+
+if parts.include?('build')
+  build += 1
+end
+
+if parts.include?('date')
+  date = now_as_date
+end
+
+if ( minor > 99 )
+  major += 1
+  minor = 0
+end
+
+if ( major > 99 )
+  version += 1
+  major = 0
+  minor = 0
 end
 
 if ( version > 99 )
-    release += 1
-    version = 0
-    build = 0
+  raise "version numbers exhausted"
 end
 
-if ( release > 99 )
-    release = 0
-    version = 0
-    build = 0
+output = <<END
+version=#{version}.#{major}.#{minor}
+build=#{build}
+date=#{date}
+END
+
+File.open(target_file_name, 'w') do |f|
+  f.puts output
 end
 
-output = "version=#{release}.#{version}.#{build}"
-exit 0 if File.exist?(target_file_name) and show_only
-
-File.open(target_file_name,'w') do |f|
-    f.puts output
-    f.puts "date=#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}"
-end
 puts output
 
 exit 0
