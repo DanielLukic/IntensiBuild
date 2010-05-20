3e8d3c94399d718dbc26fa8b93810de09f8c89fd
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-05-20 18:13:51 +0200

    Fixed launching correct intent in android_start target.

---
+++ src/ruby/determine_intent.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+apk_path = ARGV.shift
+raise "missing apk argument" unless apk_path
+
+properties_path = apk_path.sub /\.apk$/, '.properties'
+properties = File.readlines(properties_path)
+
+classname_line = properties.select { |line| line =~ /midlet\.classname/ }.first.chomp
+package_line = properties.select { |line| line =~ /midlet\.package/ }.first.chomp
+
+classname = classname_line.sub /.*=[ \t]*/, ''
+package = package_line.sub /.*=[ \t]*/, ''
+
+puts "#{package}/.#{classname}"
