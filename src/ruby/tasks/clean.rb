deec6eb8a5865199bd3082f976409efe4c043b95
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-06-19 10:07:27 +0200

    Changed clean and clobber rake tasks to use dependencies.

--- src/ruby/tasks/clean.rb
+++ src/ruby/tasks/clean.rb
@@ -1,3 +1,23 @@
+CLEAN_LIST = %w(release.log release.properties)
+
+desc "Clean all temporary and generated files."
+task :clean do
+  require 'fileutils'
+  CLEAN_LIST.each { |folder| FileUtils.rm_rf folder }
+end
+
+desc "Clean all generated files - including the really expensive ones."
+task :clobber => :clean do
+  require 'fileutils'
+  CLOBBER_FOLDERS.each { |folder| FileUtils.rm_rf folder }
+end
+
+task :clean => 'clean:build'
+#task :clean => 'clean:font_size_files'
+
+task :clobber => 'clean:release'
+task :clobber => 'clean:modules'
+
 namespace :clean do
 
   desc "Remove the build folder."
