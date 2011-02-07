ff5485fdea1dabeb26234e0931fa70191aac486a
Daniel Lukic <daniel.lukic@berlinfactor.com>
2011-02-07 21:47:49 +0100

    Added (broken) FEINT support.

--- src/ruby/build.rb
+++ src/ruby/build.rb
@@ -89,6 +89,7 @@ def write_release_properties( config, target, screen_size )
       config.keep.each do |classname_with_package|
         file.puts "-keep class * extends #{classname_with_package}"
         file.puts "-keep class #{classname_with_package} { public static <fields>; }"
+        file.puts "-keep class net.intensicode.IntensiApp"
       end
     end
 
