7307978be84ff53d3877cc5f1db55ee7d9f42e91
Daniel Lukic <daniel.lukic@berlinfactor.com>
2011-06-01 17:54:34 +0200

    Updated rake modules tasks to use git submodule now.

--- src/ruby/tasks/modules.rb
+++ src/ruby/tasks/modules.rb
@@ -1,44 +1,23 @@
 namespace :modules do
 
-  def git_modules
-    return git_config.modules if git_config.has_modules?
-    raise "no git modules defined - is #{Git::Configuration::GITMODULES_PATH} missing?"
-  end
-
-  def git_config
-    require 'git/configuration'
-    $git_config ||= Git::Configuration.load
-  end
-
-  def git
-    require 'git/system'
-    $git ||= Git::System.new git_config
-  end
-
-  def show_header(git_module)
-    puts
-    puts git_module
-    puts git_module.to_s.gsub /./, '*'
-  end
-
   desc "Initialize the GIT sub-modules if necessary."
   task :init do
-    git_modules.each { |m| show_header m ; git.init_module(m) }
+    run "git submodule init"
   end
 
   desc "Update the GIT sub-modules if necessary."
   task :update do
-    git_modules.each { |m| show_header m ; git.update_module(m) }
+    run "git submodule update"
   end
 
   desc "Show status of the GIT sub-modules."
   task :status do
-    git_modules.each { |m| show_header m ; git.module_status(m) }
+    run "git submodule foreach git status"
   end
 
   desc "Stash changes in the GIT sub-modules."
   task :stash do
-    git_modules.each { |m| show_header m ; git.stash_module(m) }
+    run "git submodule foreach git stash"
   end
 
 end
